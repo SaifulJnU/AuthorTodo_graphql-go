2a8993e4b4b048ddfe27e3e0f2b65bda49fe2942
SaifulJnU <saiful2561998@gmail.com>
2023-09-20 18:01:52 +0600

    update handler.go

--- todoql/handler.go
+++ todoql/handler.go
@@ -7,9 +7,9 @@ import (
 	"github.com/graphql-go/handler"
 )
 
-// AuthorGraphQLHandler returns an HTTP handler for the Author GraphQL schema.
+// This returns an HTTP handler for the Author GraphQL schema.
 func AuthorGraphQLHandler(ac AuthorController) http.Handler {
-	// Create a new schema using the NewAuthorSchema function from schema.go
+	
 	authorCollection := ac.db.Database("todoDB").Collection("author")
 	authorTodoCollection := ac.db.Database("todoDB").Collection("todos")
 
@@ -19,7 +19,7 @@ func AuthorGraphQLHandler(ac AuthorController) http.Handler {
 	graphQLHandler := handler.New(&handler.Config{
 		Schema: &authorSchema,
 		Pretty: true,
-		//GraphiQL: true, // enable the GraphiQL web interface
+		//GraphiQL: true, //GraphiQL for web interface
 	})
 
 	// Pass the authorCollection to the resolver functions
@@ -28,22 +28,19 @@ func AuthorGraphQLHandler(ac AuthorController) http.Handler {
 	return graphQLHandler
 }
 
-// TodoGraphQLHandler returns an HTTP handler for the Todo GraphQL schema.
+
+
+
+
 // func TodoGraphQLHandler(td TodoController) http.Handler {
-// 	// Create a new schema using the NewSchema function from schema.go
 // 	todoCollection := td.db.Database("todoDB").Collection("todos")
 // 	todoSchema := NewTodoSchema()
-
-// 	// Create a new GraphQL handler for the Todo schema
 // 	graphQLHandler := handler.New(&handler.Config{
 // 		Schema: &todoSchema,
-// 		Pretty: true, // Set this to true for pretty JSON responses in the playground
-// 		//GraphiQL: true, // Set this to true to enable the GraphiQL web interface
+// 		Pretty: true,
+// 		//GraphiQL: true, 
 // 	})
-
-// 	// Pass the todoCollection to the resolver functions
 // 	SetTodoCollection(todoCollection)
 // 	//SetAuthorTodoCollection(todoCollection)
-
 // 	return graphQLHandler
 // }
