69c2b47a527c0fade8320840a5cb66903af85413
SaifulJnU <saiful2561998@gmail.com>
2023-09-20 18:05:17 +0600

    clean code

--- todoql/http.go
+++ todoql/http.go
@@ -4,16 +4,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// //////////for author controller///////////////
-type TodoController struct {
-	db *mongo.Client
-}
-
-func NewTodoController(db *mongo.Client) *TodoController {
-	return &TodoController{
-		db: db,
-	}
-}
 
 type AuthorController struct {
 	db *mongo.Client
